7daef9209ca683f7903b3eddd7fc62e606852d08
emmohan290997 <emmohan97@gmail.com>
2025-01-02 15:59:55 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,45 @@
+const app = require("express")();
+
+const path = require("path");
+
+const { open } = require("sqlite");
+const sqlite3 = require("sqlite3");
+
+const dbPath = path.join(__dirname, "database.db");
+
+let db = null;
+
+const initializeDBAndServer = async () => {
+  try {
+    db = await open({
+      filename: dbPath,
+      driver: sqlite3.Database,
+    });
+    app.listen(3000, () => {
+      console.log("Server Running at http://localhost:3000/");
+    });
+  } catch (e) {
+    console.log(`DB Error: ${e.message}`);
+    process.exit(1);
+  }
+};
+
+initializeDBAndServer();
+
+app.post("/login", async (request, response) => {
+  const { email, password } = request.body;
+  const selectUserQuery = `SELECT * FROM UserDetails WHERE email = '${email}'`;
+  const dbUser = await db.get(selectUserQuery);
+  if (dbUser === undefined) {
+    response.status(400);
+    response.send("Invalid User");
+  } else {
+    const isPasswordMatched = await bcrypt.compare(password, dbUser.Password);
+    if (isPasswordMatched === true) {
+      response.send("Login Success!");
+    } else {
+      response.status(400);
+      response.send("Invalid Password");
+    }
+  }
+});
